1444ca77e33ff55792de731f2bc10d82486bf812
allen <theallenma@gmail.com>
2020-05-08 23:22:02 -0700

    Connect DB and set up routes

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const connectDb = require('./config/db');
 
 const app = express();
 
+// Connect database
+connectDb();
+
 app.get('/', (req, res) => res.send('API running...'));
+
+// Define routes
+app.use('/api/user', require('./routes/user'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/post', require('./routes/post'));
+app.use('/api/profile', require('./routes/profile'));
 
 const PORT = process.env.PORT || 5000;
 
