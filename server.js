b6e08d972063797349ef3b075a476d40c63c312a
allen <theallenma@gmail.com>
2020-05-16 23:20:02 -0700

    User register and login

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ const app = express();
 // Connect database
 connectDb();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running...'));
 
 // Define routes
-app.use('/api/user', require('./routes/user'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/post', require('./routes/post'));
-app.use('/api/profile', require('./routes/profile'));
+app.use('/api/user', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
